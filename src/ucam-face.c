d398b09048ab0c049ca67c8efc79f8139330754f
Rich Wareham <rjw57@cam.ac.uk>
2013-11-14 13:05:56 +0000

    re-indent source (whitespace only change)

--- src/ucam-face.c
+++ src/ucam-face.c
@@ -5,54 +5,54 @@ static TextLayer *text_layer;
 
 // Called once per minute
 static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
-  static char time_text[] = "00:00"; // Needs to be static because it's used by the system later.
-  strftime(time_text, sizeof(time_text), "%H:%M", tick_time);
-  text_layer_set_text(text_layer, time_text);
+    static char time_text[] = "00:00"; // Needs to be static because it's used by the system later.
+    strftime(time_text, sizeof(time_text), "%H:%M", tick_time);
+    text_layer_set_text(text_layer, time_text);
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Hello, world");
-  text_layer_set_text_color(text_layer, GColorWhite);
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  text_layer_set_background_color(text_layer, GColorClear);
+    text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+    text_layer_set_text(text_layer, "Hello, world");
+    text_layer_set_text_color(text_layer, GColorWhite);
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    text_layer_set_background_color(text_layer, GColorClear);
 
-  // Ensures time is displayed immediately (will break if NULL tick event accessed).
-  // (This is why it's a good idea to have a separate routine to do the update itself.)
-  time_t now = time(NULL);
-  struct tm *current_time = localtime(&now);
-  handle_minute_tick(current_time, MINUTE_UNIT);
-  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
+    // Ensures time is displayed immediately (will break if NULL tick event accessed).
+    // (This is why it's a good idea to have a separate routine to do the update itself.)
+    time_t now = time(NULL);
+    struct tm *current_time = localtime(&now);
+    handle_minute_tick(current_time, MINUTE_UNIT);
+    tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
 
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+    text_layer_destroy(text_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_background_color(window, GColorBlack);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  window_stack_push(window, /* animated = */ true);
+    window = window_create();
+    window_set_background_color(window, GColorBlack);
+    window_set_window_handlers(window, (WindowHandlers) {
+            .load = window_load,
+            .unload = window_unload,
+            });
+    window_stack_push(window, /* animated = */ true);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+    window_destroy(window);
 }
 
 int main(void) {
-  init();
+    init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_event_loop();
-  deinit();
+    app_event_loop();
+    deinit();
 }
